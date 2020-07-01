7565c63d2fd47e6f2863519878af7e567a704ce1
Salinda <salinda.jayasundara3010@gmail,com>
2020-07-01 21:30:27 +0530

    final commit

---
+++ EmguCv Tutorial 05/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace EmguCv_Tutorial_05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.histogramBox1 = new Emgu.CV.UI.HistogramBox();
+            this.histogramBox2 = new Emgu.CV.UI.HistogramBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnGray = new System.Windows.Forms.Button();
+            this.btnHisto01 = new System.Windows.Forms.Button();
+            this.btnHisto02 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(438, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(340, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "EmguCV Tutorial 05";
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BackColor = System.Drawing.Color.White;
+            this.imageBox1.Location = new System.Drawing.Point(125, 57);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(405, 330);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // histogramBox1
+            // 
+            this.histogramBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.histogramBox1.Location = new System.Drawing.Point(750, 69);
+            this.histogramBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.histogramBox1.Name = "histogramBox1";
+            this.histogramBox1.Size = new System.Drawing.Size(492, 283);
+            this.histogramBox1.TabIndex = 4;
+            // 
+            // histogramBox2
+            // 
+            this.histogramBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.histogramBox2.Location = new System.Drawing.Point(750, 431);
+            this.histogramBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.histogramBox2.Name = "histogramBox2";
+            this.histogramBox2.Size = new System.Drawing.Size(492, 283);
+            this.histogramBox2.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(125, 406);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(405, 341);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(12, 161);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(91, 53);
+            this.btnOpen.TabIndex = 7;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnGray
+            // 
+            this.btnGray.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnGray.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGray.Location = new System.Drawing.Point(12, 536);
+            this.btnGray.Name = "btnGray";
+            this.btnGray.Size = new System.Drawing.Size(91, 54);
+            this.btnGray.TabIndex = 8;
+            this.btnGray.Text = "Gray";
+            this.btnGray.UseVisualStyleBackColor = false;
+            this.btnGray.Click += new System.EventHandler(this.Gray_Click);
+            // 
+            // btnHisto01
+            // 
+            this.btnHisto01.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnHisto01.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHisto01.Location = new System.Drawing.Point(590, 174);
+            this.btnHisto01.Name = "btnHisto01";
+            this.btnHisto01.Size = new System.Drawing.Size(110, 53);
+            this.btnHisto01.TabIndex = 9;
+            this.btnHisto01.Text = "Histogram 01";
+            this.btnHisto01.UseVisualStyleBackColor = false;
+            this.btnHisto01.Click += new System.EventHandler(this.btnHisto01_Click);
+            // 
+            // btnHisto02
+            // 
+            this.btnHisto02.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnHisto02.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHisto02.Location = new System.Drawing.Point(599, 555);
+            this.btnHisto02.Name = "btnHisto02";
+            this.btnHisto02.Size = new System.Drawing.Size(101, 55);
+            this.btnHisto02.TabIndex = 10;
+            this.btnHisto02.Text = "Histogram 02";
+            this.btnHisto02.UseVisualStyleBackColor = false;
+            this.btnHisto02.Click += new System.EventHandler(this.btnHisto02_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1273, 759);
+            this.Controls.Add(this.btnHisto02);
+            this.Controls.Add(this.btnHisto01);
+            this.Controls.Add(this.btnGray);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.histogramBox2);
+            this.Controls.Add(this.histogramBox1);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.HistogramBox histogramBox1;
+        private Emgu.CV.UI.HistogramBox histogramBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnGray;
+        private System.Windows.Forms.Button btnHisto01;
+        private System.Windows.Forms.Button btnHisto02;
+    }
+}
+
---
+++ EmguCv Tutorial 05/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace EmguCv_Tutorial_05
+{
+    public partial class Form1 : Form
+    {
+
+        Image<Bgr, Byte> InputImage;
+        Image<Gray, Byte> GrayImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Gray_Click(object sender, EventArgs e)
+        {
+            GrayImage = InputImage.Convert<Gray, Byte>();
+            pictureBox1.Image = GrayImage.Bitmap;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            String FileName = "D:\\Visual Studio 2019 workspace\\6a.png";
+            InputImage = new Image<Bgr, byte>(FileName);
+
+            if (InputImage == null)
+            {
+                MessageBox.Show("Failed to Read Image....!");
+                return;
+            }
+
+            imageBox1.Image = InputImage;
+        }
+
+        private void btnHisto01_Click(object sender, EventArgs e)
+        {
+            DenseHistogram hist = new DenseHistogram(256, new RangeF(0, 255));
+            hist.Calculate(new Image<Gray, Byte>[] { InputImage[0] }, false, null);
+
+            Mat m = new Mat();
+            hist.CopyTo(m);
+
+            histogramBox1.AddHistogram("Blue Channel Histogram", Color.Blue, m, 256, new float[] { 0, 256 });
+            histogramBox1.Refresh();
+        }
+
+        private void btnHisto02_Click(object sender, EventArgs e)
+        {
+            DenseHistogram hist = new DenseHistogram(256, new RangeF(0, 255));
+            hist.Calculate(new Image<Gray, Byte>[] { GrayImage }, false, null);
+
+            Mat m = new Mat();
+            hist.CopyTo(m);
+
+            histogramBox2.AddHistogram("Gray Scale Histogram", Color.Blue, m, 256, new float[] { 0, 256 });
+            histogramBox2.Refresh();
+        }
+    }
+}
